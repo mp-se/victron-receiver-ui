f913b1e7b70581d53eee85e3d09f1875821addeb
GitHub Action <mp-se@noreply.github.com>
2024-10-01 07:03:49 +0000

    GitHub Action Build

--- dist/assets/index.js
+++ dist/assets/index.js
@@ -6628,7 +6628,8 @@ const useGlobalStore = /* @__PURE__ */ defineStore("global", {
       messageWarning: "",
       messageSuccess: "",
       messageInfo: "",
-      fetchTimout: 8e3
+      fetchTimout: 8e3,
+      url: void 0
     };
   },
   getters: {
@@ -6648,13 +6649,19 @@ const useGlobalStore = /* @__PURE__ */ defineStore("global", {
       return "Bearer " + this.id;
     },
     baseURL() {
-      return window.location.href;
+      if (this.url !== void 0)
+        return this.url;
+      {
+        logInfo("configStore:baseURL()", "Using base URL from env", window.location.href);
+        this.url = window.location.href;
+      }
+      return this.url;
     },
     uiVersion() {
-      return "0.1.0";
+      return "1.1.2";
     },
     uiBuild() {
-      return "..9c4db2";
+      return "..c3f4dc";
     },
     disabled32() {
       if (this.disabled) return true;
