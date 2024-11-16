7268e786892d48da94481aa6eb5617bef68f1e3b
GitHub Action <mp-se@noreply.github.com>
2024-11-16 15:56:57 +0000

    GitHub Action Build

--- dist/assets/index.js
+++ dist/assets/index.js
@@ -6660,7 +6660,7 @@ const useGlobalStore = /* @__PURE__ */ defineStore("global", {
       return "0.2.0";
     },
     uiBuild() {
-      return "..d0ceeb";
+      return "..88c18b";
     },
     disabled32() {
       if (this.disabled) return true;
@@ -10218,7 +10218,7 @@ const _sfc_main$A = {
   setup(__props) {
     const save = () => {
       if (!validateCurrentForm()) return;
-      if (!config.http_post_target.endsWith("/"))
+      if (config.http_post_target != "" && !config.http_post_target.endsWith("/"))
         config.http_post_target += "/";
       global$1.clearMessages();
       if (!config.http_post_header2.startsWith("Authorization: Bearer")) {
